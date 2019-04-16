c78f235256f6ca9c67a9b4e0edb25c8dccd3b329
tom_glb <714130030@qq_glb.com>
2019-04-16 21:15:18 +0800

    删除多余jar包

--- src/main/java/com/jt/controller/RequestHandleController.java
+++ src/main/java/com/jt/controller/RequestHandleController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import jdk.nashorn.internal.ir.RuntimeNode.Request;
 @RequestMapping("/reg/")
 @Controller
 public class RequestHandleController {

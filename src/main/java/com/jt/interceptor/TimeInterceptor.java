77a45aa7c98cf64675f69d5bff28d30987e6e67c
tom_glb <714130030@qq_glo.com>
2019-04-18 11:02:12 +0800

    时间监控

---
+++ src/main/java/com/jt/interceptor/TimeInterceptor.java
@@ -0,0 +1,41 @@
+package com.jt.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+/**
+ * 自定义拦截器对象
+ * @author Administrator
+ *
+ */
+public class TimeInterceptor implements HandlerInterceptor {
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		System.out.println("preHandle");
+		long startTime=System.currentTimeMillis();
+		request.setAttribute("startTime", startTime);
+		return true;//true表示放行，false表示拦截,不在执行后续拦截器后控制层方法
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		System.out.println("postHandle");
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		long endTime=System.currentTimeMillis();
+		System.out.println("afterCompletion");
+		long startTime=(Long) request.getAttribute("startTime");
+		System.out.println(startTime);
+		System.out.println(endTime-startTime);
+		
+	}
+
+}
